d43c64b4722f4cfe36b43f1841fd82e3433b3a6d
Vince <vince9458@yahoo.com.tw>
2017-09-03 14:32:25 +0800

    OrderList realtime update

--- TimeForColdDrinks/Order/OrderListTableViewController.swift
+++ TimeForColdDrinks/Order/OrderListTableViewController.swift
@@ -37,6 +37,7 @@ class orderListTableViewController: UITableViewController {
             print("complete fetch")
             self.allUid = newAllUid
             print(self.allUid)
+            self.viewWillAppear(true)
         }
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
